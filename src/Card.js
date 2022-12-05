86537ced27f5533dda0c2d7f61e8b574d0879217
Ao-chi <106396818+Ao-chi@users.noreply.github.com>
2022-12-05 16:32:57 +0800

    First commit

---
+++ src/Card.js
@@ -0,0 +1,43 @@
+import eth from "./assets/images/icon-ethereum.svg";
+import clock from "./assets/images/icon-clock.svg";
+import overlay from "./assets/images/icon-view.svg";
+import usericon from "./assets/images/image-avatar.png";
+const image = require("./assets/images/image-equilibrium.jpg");
+
+const Card = () => {
+    return (
+        <div className="card">
+            <div className="card-img">
+                <img className="image" src={image} alt="imageequilibrium" />
+                <img className="overlay-img" src={overlay} alt="icon view" />
+                <div className="card-img-overlay"></div>
+            </div>
+            <div className="card-body">
+                <div className="card-title">
+                    <h2>Equilibrium #3429</h2>
+                </div>
+                <div className="card-desc">
+                    <p>Our Equilibrium collection promotes balance and calm.</p>
+                </div>
+                <div className="stats">
+                    <div className="eth">
+                        <img src={eth} alt="eth" />
+                        <p>0.041ETH</p>
+                    </div>
+                    <div className="days">
+                        <img src={clock} alt="time icon" />
+                        <p>3 days left</p>
+                    </div>
+                </div>
+                <div className="card-user">
+                    <img src={usericon} alt="user icon" />
+                    <p>
+                        Creation of <a href="#">Jules Wyvern</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
